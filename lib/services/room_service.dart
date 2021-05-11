b8ab0546301f4b6d4eedd9a737965ee2e15852d6
haqqer <ilhamserbaguna@gmail.com>
2021-05-11 19:52:22 +0700

    add few service

--- lib/services/room_service.dart
+++ lib/services/room_service.dart
@@ -1,46 +1,63 @@
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:shareloc/models/room.dart';
 import 'package:shareloc/models/room_participant.dart';
 import 'package:shareloc/utils/request.dart';
 
-Future<Map<String, dynamic>> postRoomAndJoin(
-    Room room, RoomParticipant roomParticipant) async {
-  try {
-    var response = await request().post('/room/join',
-        data: json.encode({
-          'name': room.name,
-          'desc': room.desc,
-          'username': roomParticipant.name,
-          'status': roomParticipant.status,
-          'latitude': roomParticipant.latitude,
-          'longitude': roomParticipant.longitude
-        }));
-    return response.data['data'];
-  } catch (e) {
-    print(e);
-    return <String, dynamic>{'error': 'intinte error'};
+class RoomService {
+  static Future<Map<String, dynamic>> postRoomAndJoin(
+      Room room, RoomParticipant roomParticipant) async {
+    try {
+      var response = await request().post('/room/join',
+          data: json.encode({
+            'name': room.name,
+            'desc': room.desc,
+            'username': roomParticipant.name,
+            'status': roomParticipant.status,
+            'latitude': roomParticipant.latitude,
+            'longitude': roomParticipant.longitude
+          }));
+      return response.data['data'];
+    } catch (e) {
+      print(e);
+      return <String, dynamic>{'error': 'intinte error'};
+    }
   }
-}
 
-Future<Map<String, dynamic>> postJoinRoom(
-    String code, RoomParticipant roomParticipant) async {
-  try {
-    var response = await request()
-        .post('/room/code/' + code, data: roomParticipant.toJson());
-    return response.data['data'];
-  } catch (e) {
-    print(e);
-    return <String, dynamic>{'error': e.toString()};
+  static Future<bool> getRoomExist(String roomCode) async {
+    try {
+      var response = await request().get('/room/code/' + roomCode);
+      print(response.statusCode);
+      return true;
+    } on DioError catch (e) {
+      if (e.response.statusCode == 404) {
+        return false;
+      }
+      return false;
+    }
   }
-}
 
-Future<Map<String, dynamic>> getRoomParticipantsUpdate(String code) async {
-  try {
-    var response = await request().get('/room/code/' + code);
-    return response.data['data'];
-  } catch (e) {
-    print(e);
-    return <String, dynamic>{'error': e.toString()};
+  static Future<Map<String, dynamic>> postJoinRoom(
+      String code, RoomParticipant roomParticipant) async {
+    try {
+      var response = await request()
+          .post('/room/code/' + code, data: roomParticipant.toJson());
+      return response.data['data'];
+    } catch (e) {
+      print(e);
+      return <String, dynamic>{'error': e.toString()};
+    }
+  }
+
+  static Future<Map<String, dynamic>> getRoomParticipantsUpdate(
+      String code) async {
+    try {
+      var response = await request().get('/room/code/' + code);
+      return response.data['data'];
+    } catch (e) {
+      print(e);
+      return <String, dynamic>{'error': e.toString()};
+    }
   }
 }
